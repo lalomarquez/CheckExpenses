7a5a6a0e3ab0bd4994e7ddcd4cdfc0a3b8dc68db
eduardo markez <alfa_8581@hotmail.com>
2018-05-09 10:28:32 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../styles/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Check Expenses</h1>
+        </header>
+        <p>hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
